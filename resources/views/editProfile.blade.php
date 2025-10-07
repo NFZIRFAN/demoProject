bfd6146c3b64f1458313a3bf0d7a014a2423b5ea
NFZIRFAN <nafizirfan03@gmail.com>
2025-10-07 23:57:22 +0800

    import flowbite in app.js

--- resources/views/editProfile.blade.php
+++ resources/views/editProfile.blade.php
@@ -6,6 +6,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css" rel="stylesheet"/>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js"></script>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
 <style>
  body {
@@ -498,6 +500,28 @@ footer .bottom-bar {
   </div>
 </div>
 
+
+<footer class="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li>
+    </ul>
+    </div>
+</footer>
+
 <!-- FOOTER -->
 <footer>
   <div class="bottom-bar">
@@ -524,6 +548,7 @@ footer .bottom-bar {
     </div>
   </div>
 </div>
+<script src="../node_modules/flowbite/dist/flowbite.min.js"></script>
 
 <script>
 document.addEventListener('DOMContentLoaded', function () {
@@ -695,8 +720,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 </script>
-
-
+<script src="../node_modules/flowbite/dist/flowbite.min.js"></script>
 
 </body>
 </html>
