d66145bd00e7edd40e5b35222e51b1c048de5b18
NFZIRFAN <nafizirfan03@gmail.com>
2025-10-07 22:21:10 +0800

    navbar component

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -115,23 +115,7 @@ footer .bottom-bar {
 </head>
 <body class="min-h-screen flex flex-col">
 
-<!-- ===== Navbar ===== -->
-<nav class="navbar">
-  <div class="nav-left">
-    <a href="{{ route('customer.dashboard') }}" class="logo-text">🌿 YAH NURSERY</a>
-  </div>
-  <ul class="nav-links">
-    <li><a href="{{ route('customer.dashboard') }}" class="{{ request()->routeIs('customer.dashboard') ? 'active' : '' }}">Dashboard</a></li>
-    <li><a href="{{ route('customer.profile') }}" class="{{ request()->routeIs('customer.profile') ? 'active' : '' }}">My Profile</a></li>
-    <li><a href="{{ route('customer.profile.edit') }}" class="{{ request()->routeIs('customer.profile.edit') ? 'active' : '' }}">Edit Profile</a></li>
-<li>
-      <a href="{{ route('cart.view') }}" 
-         class="{{ request()->routeIs('cart.view') ? 'active' : '' }}">
-         Cart
-      </a>
-    </li>    <li><a href="{{ route('customer.logout') }}">Logout</a></li>
-  </ul>
-</nav>
+<x-navbar />
 
 <div class="container max-w-7xl mx-auto p-4 md:p-8 flex flex-col lg:flex-row gap-8 flex-grow">
 
