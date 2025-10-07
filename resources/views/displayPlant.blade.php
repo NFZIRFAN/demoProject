7d954893d26759902cde782a4fde3e0b74487ac7
NFZIRFAN <nafizirfan03@gmail.com>
2025-10-07 23:42:52 +0800

    add navbar layout into display plant view

--- resources/views/displayPlant.blade.php
+++ resources/views/displayPlant.blade.php
@@ -356,22 +356,7 @@
 </head>
 <body>
   <!-- ===== Navbar ===== -->
-<nav class="navbar">
-  <div class="nav-left">
-    <a href="{{ route('customer.dashboard') }}" class="logo-text">🌿 YAH NURSERY</a>
-  </div>
-  <ul class="nav-links">
-    <li><a href="{{ route('customer.dashboard') }}" class="{{ request()->routeIs('customer.dashboard') ? 'active' : '' }}">Dashboard</a></li>
-    <li><a href="{{ route('customer.profile') }}" class="{{ request()->routeIs('customer.profile') ? 'active' : '' }}">My Profile</a></li>
-    <li><a href="{{ route('customer.profile.edit') }}" class="{{ request()->routeIs('customer.profile.edit') ? 'active' : '' }}">Edit Profile</a></li>
-<li>
-      <a href="{{ route('cart.view') }}" 
-         class="{{ request()->routeIs('cart.view') ? 'active' : '' }}">
-         Cart
-      </a>
-    </li>    <li><a href="{{ route('customer.logout') }}">Logout</a></li>
-  </ul>
-</nav>
+<x-navbar />
   <!-- CONTENT WRAPPER -->
   <div class="content-main-wrapper">
     <!-- top-right cart badge (Adjusted position to clear fixed navbar) -->
